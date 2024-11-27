d7d3a41a018d8b378ff74cff78ce730e312f1d2f
IvanSamdi <ivan.malin2014@gmail.com>
2024-11-27 16:29:21 +0300

    Add files via upload

--- module_7_1.py
+++ module_7_1.py
@@ -3,9 +3,9 @@ from pprint import pprint
 
 class Product:
     def __init__(self, name, weight, category):
-        self.name = str(name)
-        self.weight = float(weight)
-        self.category = str(category)
+        self.name = name
+        self.weight = weight
+        self.category = category
 
     def __str__(self):
         return f'{self.name}, {self.weight}, {self.category}'
